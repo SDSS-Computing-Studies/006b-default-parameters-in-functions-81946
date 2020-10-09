45a0cf6d21c1d7d81bf2e5946a089fc42afb8be5
81946 <71406891+81946@users.noreply.github.com>
2020-10-09 12:59:22 -0700

    First draft

--- assignment.py
+++ assignment.py
@@ -2,18 +2,50 @@
 
 import math
 
-def tempConversion():
+def tempConversion(degrees, unit):
+    if unit=="C":
+        answer= (degrees*9/5)+32
+        answer=float(answer)
+        return answer
+    if unit=="F":
+        answer= (degrees-32)*5/9
+        answer=float(answer)
+        return answer
 
 
-def factorPair():
+#Factor Pair
+#In the assignment.py file:
+#Define a function called factorPair().
+#It has 2 required integer inputs:
+#a number, and a factor
+#The return value is a sorted list of the 2 factors that multiply to 
+#make the number
 
+#Example Command:
+#print( factorPair(10,2) )
+#expects:
+#[2, 5]
+
+def factorPair(number, factor):
+    import math
+    i= number/factor
+    if number % i == 0:
+        lists=[i,factor]
+        print( lists ) 
+           
+
+    
 
 
 def cosineLaw():
+    pass
 
 def convertAngle():
+    pass
 
 def solution():
+    pass
 
 def quadratic():
+    pass
 
--- task2.py
+++ task2.py
@@ -1,2 +1,5 @@
 #!python3
 
+import assignment
+
+print( assignment.factorPair(40,1) )
